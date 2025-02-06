00529f8d497f92db42bda6941fd19fd817687e88
jones.moura <jones-ms@live.com>
2025-02-06 19:06:10 -0300

    feat: Configuração inicial do ORM
    - Configuração inicial do entity framework

---
+++ Sistema De Orçamentos/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaOrc.Models;
+
+namespace SistemaOrc.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base()
+        {
+            
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Orcamento> Orcamentos { get; set; }
+        public DbSet<Servico> Servicos { get; set; }
+
+    }
+}
--- Sistema De Orçamentos/Program.cs
+++ Sistema De Orçamentos/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using MySql.EntityFrameworkCore;
+using SistemaOrc.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
